89dd4ef06cfb5e889d811648650df1174e8b6d35
td59210 <td59210@mail.harpercollege.edu>
2026-02-15 22:36:36 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -53,14 +53,14 @@ public:
 
 	//updating stats
 	//adding
-	void addHealth(int amount) {
-		health += amount;
+	void addHealth(int x) {
+		health += x;
 	}
-	void addStrength(int amount) {
-		strength += amount;
+	void addStrength(int x) {
+		strength += x;
 	}
-	void addDefense(int amount) {
-		defense += amount;
+	void addDefense(int x) {
+		defense += x;
 	}
 	//damage
 	void takeDamage(int damage) {
@@ -89,108 +89,6 @@ public:
 		cout << "Strength: " << strength << endl;
 		cout << "Defense: " << defense << endl;
 		cout << "Keys: " << keys << endl;
-	}
-
-};
-
-class Dungeon {
-private:
-	vector<string> grid; //vector<vector<char>> grid;
-	int rows;
-	int cols;
-
-public:
-	Dungeon(vector<string> levelGrid) { //takes in chosen grid
-		grid = levelGrid;
-		rows = grid.size();
-		cols = grid[0].size();
-	}
-
-	void print(Player& player) {
-		cout << "\n";
-
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < cols; j++) {
-				if (i == player.getRow() && j == player.getCol())
-					cout << "@";
-				else
-					cout << grid[i][j];
-			}
-			cout << endl;
-		}
-
-		//display stats
-		cout << "\nHealth: " << player.getHealth();
-		cout << "\nStrength: " << player.getStrength();
-		cout << "\nDefense: " << player.getDefense();
-		cout << "\nKeys: " << player.getKeys();
-	}
-	//check bounds
-	bool inBounds(int r, int c) {
-		return r >= 0 && r < rows && c >= 0 && c < cols;
-	}
-
-	//movement
-	bool movePlayer(Player& player, char dir) {
-		int newRow = player.getRow(); //initialize new pos
-		int newCol = player.getCol();
-
-		if (dir == 'U' || dir == 'u')
-			newRow--;
-		else if (dir == 'D' || dir == 'd')
-			newRow++;
-		else if (dir == 'L' || dir == 'l')
-			newCol--;
-		else if (dir == 'R' || dir == 'r')
-			newCol++;
-
-		//check bounds
-		if (!inBounds(newRow, newCol))
-			return false;
-
-		//checking spaces and outputting info
-		char tile = grid[newRow][newCol];
-
-		if (tile == '#') {
-			cout << "You've hit a wall!\n";
-			return false;
-		}
-
-		if (tile == 'K') {
-			cout << "You've found a key!\n";
-			player.addKey();
-			grid[newRow][newCol] = '.'; //replaces item with empty space or .
-			player.printStats();
-		}
-
-		if (tile == 'D') { //door
-			if (player.useKey()) {
-				cout << "You've unlocked the door!\n";
-				grid[newRow][newCol] = '.';
-				player.printStats();
-			}
-			else {
-				cout << "You need a key.\n";
-				return false;
-			}
-		}
-
-		if (tile == 'H') {
-			cout << "You've found a health potion!\n";
-			player.addHealth(10);
-			grid[newRow][newCol] = '.';
-			player.printStats();
-			//cout << "Your new health is: " << player.getHealth();
-		}
-
-		if (tile == 'S') {
-			cout << "You've found a strength potion!\n";
-			player.addStrength(5);
-			grid[newRow][newCol] = '.';
-			player.printStats();
-		}
-
-		if (tile == defense)
 	}
 
 };
@@ -228,25 +126,322 @@ public:
 	bool isAlive() {
 		return health > 0;
 	}
-	
+
+};
+
+class Dungeon {
+private:
+	vector<string> grid; //vector<vector<char>> grid;
+	int rows;
+	int cols;
+
+public:
+	Dungeon(vector<string> levelGrid) { //takes in chosen grid
+		grid = levelGrid;
+		rows = grid.size();
+		cols = grid[0].size();
+	}
+
+	void findStart(Player& player) { //player will always start on the @
+		for (int r = 0; r < grid.size(); r++) {
+			for (int c = 0; c < grid[r].size(); c++) {
+				if (grid[r][c] == '@') {
+					player.setPosition(r, c);
+					grid[r][c] = '.';
+				}
+			}
+		}
+	}
+
+
+	void print(Player& player) {
+		cout << "\n";
+
+		for (int i = 0; i < rows; i++) { //moves player as @
+			for (int j = 0; j < cols; j++) {
+				if (i == player.getRow() && j == player.getCol())
+					cout << "@";
+				else
+					cout << grid[i][j];
+			}
+			cout << endl;
+		}
+		/*
+		//display stats
+		cout << "\nHealth: " << player.getHealth();
+		cout << "\nStrength: " << player.getStrength();   //->this ended up double printing, prefer other way
+		cout << "\nDefense: " << player.getDefense();
+		cout << "\nKeys: " << player.getKeys();
+		*/
+	}
+	//check bounds
+	bool inBounds(int r, int c) {
+		return r >= 0 && r < rows && c >= 0 && c < cols;
+	}
+
+	//movement
+	bool movePlayer(Player& player, char dir) {
+		int newRow = player.getRow(); //initialize new pos
+		int newCol = player.getCol();
+
+		if (dir == 'U' || dir == 'u')
+			newRow--;
+		else if (dir == 'D' || dir == 'd')
+			newRow++;
+		else if (dir == 'L' || dir == 'l')
+			newCol--;
+		else if (dir == 'R' || dir == 'r')
+			newCol++;
+
+		//check bounds
+		if (!inBounds(newRow, newCol))
+			return false;
+
+		//checking spaces and outputting info
+		char tile = grid[newRow][newCol];
+
+		if (tile == '#') {
+			cout << "You've hit a wall!\n";
+			return false;
+		}
+
+		if (tile == 'K') {
+			cout << "\n";
+			cout << "You've found a key!\n";
+			player.addKey();
+			grid[newRow][newCol] = '.'; //replaces item with empty space or .
+			cout << "\n";
+			player.printStats();
+		}
+
+		if (tile == 'D') { //door
+			if (player.useKey()) {
+				cout << "\n";
+				cout << "You've unlocked the door!\n";
+				grid[newRow][newCol] = '.';
+				cout << "\n";
+				player.printStats();
+			}
+			else {
+				cout << "\n";
+				cout << "You need a key.\n";
+				return false;
+			}
+		}
+
+		if (tile == 'H') {
+			cout << "\n";
+			cout << "You've found a health potion!\n";
+			player.addHealth(10);
+			grid[newRow][newCol] = '.';
+			cout << "\n";
+			player.printStats();
+			//cout << "Your new health is: " << player.getHealth();
+		}
+
+		if (tile == 'S') {
+			cout << "\n";
+			cout << "You've found a strength potion!\n";
+			player.addStrength(5);
+			grid[newRow][newCol] = '.';
+			cout << "\n";
+			player.printStats();
+		}
+
+		if (tile == 'F') {
+			cout << "\n";
+			cout << "You've found a defense potion!\n";
+			player.addDefense(5);
+			grid[newRow][newCol] = '.';
+			cout << "\n";
+			player.printStats();
+		}
+
+		if (tile == 'E') {
+			cout << "\n";
+			cout << "An enemy appeared! Your fight will now begin!\n";
+			Enemy enemy;
+
+			//fight
+			while (player.isAlive() && enemy.isAlive()) {
+				int dmgp = player.getStrength() - enemy.getDefense(); //calc damage enemy wil take
+				if (dmgp < 0) 
+					dmgp = 0; //no negative damage
+				enemy.takeDamage(dmgp); 
+
+				if (enemy.isAlive()) {
+					int dmge = enemy.getStrength() - player.getDefense();
+					if (dmge < 0)
+						dmge = 0;
+					player.takeDamage(dmge);
+				}
+
+				if (!player.isAlive()) {
+					cout << "\n";
+					cout << "You were defeated! The game will end.\n";
+					return true; //end game
+				}
+				cout << "\n";
+				cout << "You defeated the enemy! You may move on.\n"; //this prints multiple time--> fix if time
+			}
+			grid[newRow][newCol] = '.';
+		}
+		
+		if (tile == 'G') {
+			cout << "\n";
+			cout << "You've reached the goal! You win!\n";
+			player.setPosition(newRow, newCol);
+			return true; //win
+		}
+
+		player.setPosition(newRow, newCol);
+		return false;
+	}
+
+	//preloaded dungeons
+
+	static vector<vector<string>> getPreDungeon() {
+		vector<vector<string>> levels;
+
+		levels.push_back({
+			"#########",
+			"#@....E.#",
+			"#.##.##.#",
+			"#K..H..G#",
+			"#########"
+			});
+
+		levels.push_back({
+			"########",
+			"#G..#E.#",
+			"#.#D#.K#",
+			"#.#H.S.#",
+			"#@..F..#",
+			"########"
+			});
+
+		return levels;
+	}
+
 };
 
 class Game {
 private:
-
-
+	vector<vector<string>> dungeons;
 
 public:
+	
+	Game() {
+		dungeons = Dungeon::getPreDungeon();
+	}
 
+	//level editor
 
-};
+	void levelEditor() {
+		int rows, cols;
 
-class LevelEditor {
+		cout << "\n--- Level Editor ---\n";
+		cout << "Use the following characters:\n";
+		cout << "# = Wall\n";
+		cout << ". = Empty space\n";
+		cout << "@ = Player start (REQUIRED)\n";
+		cout << "K = Key\n";
+		cout << "D = Door\n";
+		cout << "H = Health potion\n";
+		cout << "S = Strength potion\n";
+		cout << "F = Defense potion\n";
+		cout << "E = Enemy\n";
+		cout << "G = Goal (REQUIRED)\n\n";
+
+		cout << "Limit rows and columns to 20.\n"; //add limit to check if time
+		cout << "Enter number of rows: ";
+		cin >> rows;
+		cout << "Enter number of columns: ";
+		cin >> cols;
+
+		vector<string> newGrid(rows);
+
+		cout << "\nEnter each row exactly " << cols << " characters long:\n";
+
+		for (int i = 0; i < rows; i++) {
+			cin >> newGrid[i];
+		}
+
+		dungeons.push_back(newGrid);
+		cout << "Dungeon created!\n";
+	}
+
+	void run() {
+		while (true) {
+			cout << "\n---Welcome to the Magic Tower---\n";
+			cout << "1) Enter a dungeon\n";
+			cout << "2) Create a dungeon\n";
+			cout << "3) Exit\n";
+
+			int choice;
+			cout << "Your choice: ";
+			cin >> choice;
+
+			if (choice == 1) {
+
+				cout << "\nChoose a dungeon:\n";
+				for (int i = 0; i < dungeons.size(); i++) {
+					cout << i + 1 << ") Dungeon " << i + 1 << endl;
+				}
+
+				int selection;
+				cin >> selection;
+				selection--;
+
+				if (selection < 0 || selection >= dungeons.size()) {
+					cout << "Invalid selection.\n";
+					continue;
+				}
+
+				Player player;
+				Dungeon dungeon(dungeons[selection]);
+
+				dungeon.findStart(player);
+
+				bool gameOver = false;
+
+				while (!gameOver && player.isAlive()) {
+					dungeon.print(player);
+
+					char move;
+					cout << "\nMove (U/D/L/R): ";
+					cin >> move;
+
+					gameOver = dungeon.movePlayer(player, move);
+				}
+
+				if (!player.isAlive()) {
+					cout << "\nYou died. Game over.\n";
+				}
+			}
+
+			else if (choice == 2) {
+				levelEditor();
+			}
+
+			else if (choice == 3) {
+				cout << "Goodbye!\n";
+				break;
+			}
+
+			else {
+				cout << "Invalid choice.\n";
+			}
+		}
+	}
 
 };
 
 
 int main() {
-
+	
+	Game game;
+	game.run();
+	return 0;
 
 }
